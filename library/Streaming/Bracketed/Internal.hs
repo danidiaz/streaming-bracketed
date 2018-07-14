093646034687464d7e571bb2836170270b2facc9
daniel <daniel@bogusemailserver.com>
2018-07-14 22:56:06 +0000

    Some simple implementations.

--- library/Streaming/Bracketed/Internal.hs
+++ library/Streaming/Bracketed/Internal.hs
@@ -17,22 +17,40 @@ newtype Bracketed a r =
 data Finstack = Finstack !Int [IO ()]  
 
 from :: Stream (Of x) IO r -> Bracketed x r
-from = undefined
+from stream = Bracketed (const stream)
 
 bracket :: IO a -> (a -> IO ()) -> (a -> Stream (Of x) IO r) -> Bracketed x r
-bracket = undefined
+bracket allocate finalize stream = Bracketed (\finref ->
+    do let open = do a <- allocate
+                     modifyIORef' finref (\(Finstack size fins) -> Finstack (succ size) (finalize a : fins))
+                     pure a
+           close = modifyIORef' finref (\(Finstack size (fin:fins)) -> Finstack (pred size) fins)
+       a <- liftIO open
+       r <- stream a
+       liftIO (finalize a)
+       liftIO close
+       pure r)
 
 with :: Bracketed a r -> (forall x. Stream (Of a) IO x -> IO (Of b x)) -> IO (Of b r)
-with _ _ = undefined
+with (Bracketed b) f = 
+    do fins <- newIORef (Finstack 0 [])
+       f (b fins)
 
 with_ :: Bracketed a r -> (forall x. Stream (Of a) IO x -> IO b) -> IO b
-with_ _ _ = undefined
+with_ (Bracketed b) f =
+    do fins <- newIORef (Finstack 0 [])
+       f (b fins)
 
 over :: (Stream (Of a) IO r -> Stream (Of b) IO r) -> Bracketed a r -> Bracketed b r 
-over = undefined
+over transform (Bracketed b) = Bracketed (transform . b)
 
 over_ :: (Stream (Of a) IO r -> Stream (Of b) IO r') -> Bracketed a r -> Bracketed b r'
 over_ = undefined
 
 for :: Bracketed a r -> (a -> Bracketed b x) -> Bracketed b r
-for = undefined
+for (Bracketed b) f = 
+    Bracketed (\fins -> S.for (b fins) (flip (runBracketed . f) fins))
+    
+-- TODO remember to add the masks!
+-- TODO applicative and monad instances.
+
