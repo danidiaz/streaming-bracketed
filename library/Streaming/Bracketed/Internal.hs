0f925cf63c029a6e4f75f552b1058276dfd77763
daniel <daniel@bogusemailserver.com>
2018-07-15 18:00:40 +0000

    comments

--- library/Streaming/Bracketed/Internal.hs
+++ library/Streaming/Bracketed/Internal.hs
@@ -26,6 +26,7 @@ instance Monad (Bracketed a) where
 
 data Finstack = Finstack !Int [IO ()]  
 
+-- | Lift a `Stream` to a `Bracketed`.
 from :: Stream (Of x) IO r -> Bracketed x r
 from stream = Bracketed (const stream)
 
@@ -41,22 +42,31 @@ bracket allocate finalize stream = Bracketed (\finref ->
           liftIO (mask (\_ -> reset size0 finref))
           pure r)
 
+-- | Consume a `Bracketed` stream, exhausting it.
 with :: Bracketed a r -> (forall x. Stream (Of a) IO x -> IO (Of b x)) -> IO (Of b r)
 with (Bracketed b) f = 
     Control.Exception.bracket (newIORef (Finstack 0 []))
                               (reset 0) 
                               (f . b)
 
+-- | Consume a `Bracketed` stream, possibly wihout exhausting it.
+--   
+--   Finalizers lying in unconsumed parts of the stream will not be executed
+--   until the callback returns, so not tarry too long if you want prompt
+--   finalization.
 with_ :: Bracketed a r -> (forall x. Stream (Of a) IO x -> IO b) -> IO b
 with_ (Bracketed b) f =
     Control.Exception.bracket (newIORef (Finstack 0 []))
                               (reset 0) 
                               (f . b)
 
+-- | Apply to the underlying stream a transformation that preserves the return value.
 over :: (Stream (Of a) IO r -> Stream (Of b) IO r) -> Bracketed a r -> Bracketed b r 
 over transform (Bracketed b) = Bracketed (transform . b)
 
-over_ :: (Stream (Of a) IO r -> Stream (Of b) IO r') -> Bracketed a r -> Bracketed b r'
+-- | Apply to the underlying stream a transformation that might not preserve
+--   the return value, for example one that takes the first N elements.
+over_ :: forall a b r r'. (Stream (Of a) IO r -> Stream (Of b) IO r') -> Bracketed a r -> Bracketed b r'
 over_ transform (Bracketed b) = Bracketed (\finref ->
     let level = do
             Finstack size _ <- readIORef finref
@@ -66,10 +76,12 @@ over_ transform (Bracketed b) = Bracketed (\finref ->
           liftIO (mask (\_ -> reset size0 finref))
           pure r)
 
+-- | for replaces each element of a stream with an associated stream. 
 for :: Bracketed a r -> (a -> Bracketed b x) -> Bracketed b r
 for (Bracketed b) f = 
     Bracketed (\fins -> S.for (b fins) (flip (runBracketed . f) fins))
     
+-- | Execute all finalizers that lay above a certain level.
 reset :: Int -> IORef Finstack -> IO ()
 reset size0 finref =
     do Finstack size fins <- readIORef finref
